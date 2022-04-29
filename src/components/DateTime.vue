0a10ee039fca515f332f6ec3c99d9696dcb6023b
balfons <alfonsbengtegard@gmail.com>
2022-04-29 19:35:29 +0200

    Date format change

--- src/components/DateTime.vue
+++ src/components/DateTime.vue
@@ -15,7 +15,7 @@ onUnmounted(() => {
 
 const time = computed(() => {
   const timeOptions: Intl.DateTimeFormatOptions = {
-    timeStyle: 'short'
+    timeStyle: 'medium'
   }
 
   return new Intl.DateTimeFormat('default', timeOptions).format(dateNow.value)
@@ -25,7 +25,8 @@ const date = computed(() => {
   const timeOptions: Intl.DateTimeFormatOptions = {
     weekday: 'short',
     day: 'numeric',
-    month: 'short'
+    month: 'short',
+    year: 'numeric'
   }
 
   return new Intl.DateTimeFormat('default', timeOptions).format(dateNow.value)
@@ -36,8 +37,10 @@ const date = computed(() => {
 
 <template>
   <div class="date-time text-center">
-    <p class="text-4xl font-bold text-neutral-900 dark:text-neutral-100">{{ time }}</p>
-    <p class="text-xl font-normal text-neutral-900 dark:text-neutral-100">{{ date }}</p>
+    <div class="inline-flex flex-col font-mono">
+      <p class="text-l text-center font-semibold text-neutral-900/50 dark:text-neutral-100/50">{{ date }}</p>
+      <p class="text-3xl font-bold text-neutral-900 dark:text-neutral-100">{{ time }}</p>
+    </div>
   </div>
 </template>
 
